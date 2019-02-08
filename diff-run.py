0bfdeba7558eca9c9d35d67a52f792e104744ecc
Nikolay Ivanov <nivanov@cfeld-macPro03.desy.de>
2019-02-08 16:45:46 +0100

    small changes

--- diff-run.py
+++ diff-run.py
@@ -8,13 +8,13 @@ from timeit import default_timer as timer
 import numpy as np
 
 if __name__ == "__main__":
-    waist = 4e-6
+    waist = 2e-5
     wavelength = 1.5e-7
     a, b, c = 2e-5, 2.5e-5, 3e-5
     Nx, Ny, Nz = 20, 20, 20
 
     detNx, detNy = 512, 512
-    pix_size = 55e-3
+    pix_size = 55e-3 / 4
     det_dist = 54
 
     axis = np.random.rand(3)
