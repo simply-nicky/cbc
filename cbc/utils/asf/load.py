d48611fa10d45ced762731a19a02b5f17645d979
Nikolay Ivanov <nivanov@cfeld-macPro03.desy.de>
2019-06-13 12:42:47 +0200

    small fixes

--- cbc/utils/asf/load.py
+++ cbc/utils/asf/load.py
@@ -1,5 +1,5 @@
 import os, numpy as np
 
 path = os.path.dirname(__file__)
-asf_henke = np.load(os.path.join(path, 'asf_henke.npy'), encoding='latin1').item()
-asf_waskif = np.load(os.path.join(path, 'asf_waskif.npy'), encoding='latin1').item()
+asf_henke = np.load(os.path.join(path, 'asf_henke.npy'), encoding='latin1', allow_pickle=True).item()
+asf_waskif = np.load(os.path.join(path, 'asf_waskif.npy'), encoding='latin1', allow_pickle=True).item()
