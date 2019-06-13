bdeef77ba4332aa49c33d23cd9bbd90b7de3a334
Nikolay Ivanov <nivanov@cfeld-macPro03.desy.de>
2019-06-13 14:29:09 +0200

    test changed

--- test.py
+++ test.py
@@ -4,21 +4,24 @@ import numpy as np
 
 if __name__ == "__main__":
     wavelength = 1.5e-7
-    waist, f, ap = 4.5e-6, 2.0, 2e-2
+    f, ap, defoc = 2, 2e-2, 1e-4
+    waist = 4.5e-6
     a, b, c = 7.9e-6, 7.9e-6, 3.8e-6
     Nx, Ny, Nz = 20, 20, 1
-
     detNx, detNy = 512, 512
     pix_size = 55e-3 / 2
     det_dist = 30
+
     knum = 20000
     zr = np.pi * waist**2 / wavelength
+    th_lens = 2 * ap / f
+    dz = Nx * a / th_lens
 
     axis = np.random.rand(3)
     theta = 2 * np.pi * np.random.random()
 
     logpath = os.path.join('logs', str(datetime.date.today()) + '.log')
-    beam = cbc.BesselBeam(waist, wavelength)
+    beam = cbc.RectBeam(waist, wavelength)
     diff = cbc.Diff(beam=beam, setup_args=cbc.SetupArgs(handler=logging.FileHandler(logpath), relpath='results/'),
                     det_args=cbc.DetArgs(det_dist=det_dist, detNx=detNx, detNy=detNy, pix_size=pix_size),
                     cell_args=cbc.CellArgs.importpdb('4et8.pdb'),
@@ -26,9 +29,9 @@ if __name__ == "__main__":
                     lat_args=cbc.LatArgs(a=a, b=b, c=c, Nx=Nx, Ny=Ny, Nz=Nz))
 
     # diff.rotate_lat(axis, theta)
-    diff.move_lat([0.5001 * a, 0.5001 * b, 0])
+    diff.move_lat([0.5001 * a, 0.5001 * b, dz])
     
-    start = timer()
+    # start = timer()
     diffres = diff.henry().pool()
     # diffres2 = diff.conv(knum).pool()
     # diffres3 = diff.nocoh(knum).pool()
