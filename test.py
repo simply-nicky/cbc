a45aab63ddecfde08bed7c984883880a3ca42484
Nikolay Ivanov <nivanov@cfeld-macPro03.desy.de>
2019-08-21 15:52:06 +0200

    small changes

--- test.py
+++ test.py
@@ -5,11 +5,14 @@ import numpy as np
 if __name__ == "__main__":
     wavelength = 1.14e-7
     # f, ap, defoc = 2, 2e-2, 1e-4
-    waist = wavelength / np.pi / 0.015
+    thdiv = 0.015
+    waist = wavelength / np.pi / thdiv
     Nx, Ny, Nz = 100, 100, 100
-    detNx, detNy = 16, 16
+    r = 2e-4
+    detNx, detNy = 2000, 2000
     pix_size = 88.6e-3
     det_dist = 250
+    dz = Nx * np.sqrt(aa.dot(aa)) / thdiv
 
     astar = np.array([0.00551908483885947, -0.00294352907953398, 0.0109864094612009])
     bstar = np.array([-0.0112435046699143, 0.000431835526544485, 0.00576393741858660])
@@ -18,25 +21,15 @@ if __name__ == "__main__":
     bb = np.cross(cstar, astar) / (np.cross(cstar, astar).dot(bstar)) * 1e-7
     cc = np.cross(astar, bstar) / (np.cross(astar, bstar).dot(cstar)) * 1e-7
 
-    # th_div = 2 * ap / f
-    th_div = wavelength / np.pi / waist
-    dz = Nx * np.sqrt(aa.dot(aa)) / th_div
-
-    # axis = np.random.rand(3)
-    # theta = 2 * np.pi * np.random.random()
-
     logpath = cbc.utils.get_logpath()
     beam = cbc.GausBeam(waist, wavelength)
     diff = cbc.Diff(beam=beam, setup=cbc.Setup(handler=logging.FileHandler(logpath)),
                     detector=cbc.Detector(det_dist=det_dist, detNx=detNx, detNy=detNy, pix_size=pix_size),
-                    # cell_args=cbc.CellArgs.importpdb('4et8.pdb'),
-                    # cell_args=cbc.CellArgs(XS=np.array([0, 0.5*a]), YS=np.zeros(2), ZS=np.zeros(2), bs=np.zeros(2), elems=['Au', 'Ag']),
                     lattice=cbc.CubicLattice(a=aa, b=bb, c=cc, Nx=Nx, Ny=Ny, Nz=Nz))
 
-    # diff.rotate_lat(axis, theta)
     diff.move_lat([0, 0, dz])
     
     start = timer()
-    diffres = diff.calculate().serial()
-    # diffres.write()
+    diffres = diff.calculate().pool()
+    diffres.write()
     print('Estimated time: %fs' % (timer() - start))
