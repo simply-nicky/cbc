5623694efff0af928d82c08c723e3cbe769aeeba
simply-nicky <simply.i.nicky@gmail.com>
2020-01-09 14:52:50 +0100

    test.py changed

--- test.py
+++ test.py
@@ -9,12 +9,12 @@ NUM_AP = np.sqrt(2.5e5**2 + 3e5**2) * WL
 FOCUS = 1.
 PIX_SIZE = 75 * 1e-3
 DET_NX, DET_NY, DET_DIST = 2000, 2000, 100
-REC_BASIS = 2. * np.array([[2e5, 0, 0],
+REC_BASIS = 2.2 * np.array([[2e5, 0, 0],
                             [0, 2.5e5, 0],
                             [0, 0, 3e5]])
 REC_BASIS = REC_BASIS.dot(cbc_dp.utils.rotation_matrix(np.array([1, 0, 0]), np.radians(20)).T)
 REC_BASIS = REC_BASIS.dot(cbc_dp.utils.rotation_matrix(np.array([0, 1, 0]), np.radians(7)).T)
-LAT_R = 1e-4
+LAT_R = 5e-4
 
 def main(rec_basis=REC_BASIS, lat_r=LAT_R, num_ap=NUM_AP, focus=FOCUS, wavelength=WL,
          det_dist=DET_DIST, det_nx=DET_NX, det_ny=DET_NY, pix_size=PIX_SIZE):
