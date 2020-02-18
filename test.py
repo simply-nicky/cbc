c77e1f259398fef5674c1f067cf9dfb0d0f7c61c
simply-nicky <simply.i.nicky@gmail.com>
2020-02-18 14:04:44 +0100

    test.py fix

--- test.py
+++ test.py
@@ -2,17 +2,18 @@
 test.py - cbc and cbc_dp packages testing script
 """
 import os
-from timeit import default_timer as timer
-import argparse
-import numpy as np
-import h5py
-import cbc_dp
 
 os.environ["OMP_NUM_THREADS"] = "4" # export OMP_NUM_THREADS=4
 os.environ["OPENBLAS_NUM_THREADS"] = "4" # export OPENBLAS_NUM_THREADS=4 
 os.environ["MKL_NUM_THREADS"] = "6" # export MKL_NUM_THREADS=6
 os.environ["VECLIB_MAXIMUM_THREADS"] = "4" # export VECLIB_MAXIMUM_THREADS=4
 os.environ["NUMEXPR_NUM_THREADS"] = "6" # export NUMEXPR_NUM_THREADS=6
+
+from timeit import default_timer as timer
+import argparse
+import numpy as np
+import h5py
+import cbc_dp
 
 PIX_SIZE = 75 * 1e-3 #mm
 WL = 7.293188082141599e-08 #mm
