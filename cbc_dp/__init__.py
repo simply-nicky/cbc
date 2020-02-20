4d9b5e4cf70c24c4bad176ad48d28e3eca0a2e50
Nikolay Ivanov <simply.i.nicky@gmail.com>
2020-02-20 13:53:25 +0100

    __init__ updated

--- cbc_dp/__init__.py
+++ cbc_dp/__init__.py
@@ -4,7 +4,7 @@ Convergent crystallogrphy data processing package.
 Compatible with Python 3.X.
 """
 from . import utils, Qt
-from .data_process import Frame, open_scan, Scan1D, Scan2D, ScanST, CorrectedData
+from .data_process import Frame, open_scan, Scan1D, Scan2D, ScanST, CorrectedData, NormalizedData
 from .feat_detect import HoughLineDetector, LineSegmentDetector, FrameSetup, ScanSetup
 from .feat_detect import FrameStreaks, ScanStreaks, RecVectors
 from .grouper import Grouper, TiltGroups
