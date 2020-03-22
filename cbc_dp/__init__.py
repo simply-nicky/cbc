1b5343c3acea1530b2d27f1222a41008deb3cd03
Nikolay Ivanov <simply.i.nicky@gmail.com>
2020-03-22 15:01:49 +0100

    cbc_dp/__init__.py fixed

--- cbc_dp/__init__.py
+++ cbc_dp/__init__.py
@@ -6,7 +6,6 @@ Compatible with Python 3.X.
 from . import utils, Qt
 from .data_process import Frame, open_scan, Scan1D, Scan2D, ScanST, CorrectedData, NormalizedData
 from .feat_detect import HoughLineDetector, LineSegmentDetector, FrameSetup, ScanSetup
-from .feat_detect import FrameStreaks, ScanStreaks, RecVectors
 from .grouper import Grouper, TiltGroups
 from .model import BallLattice, RecLattice, CircModel, RectModel
-from .indexer import ScanCBI, FCBI, RCBI
+from .indexer import ScanCBI, FCBI, RCBI, FrameStreaks, ScanStreaks, RecVectors
