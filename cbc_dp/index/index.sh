cd97993a5e0faf031cc29f112a97ee114ef6b761
Nikolay Ivanov <simply_nicky@nikolays-mbp.localdomain>
2020-04-27 10:08:26 +0200

    bug fix

--- cbc_dp/index/index.sh
+++ cbc_dp/index/index.sh
@@ -3,4 +3,10 @@
 source /home/nivanov/conda-setup.sh
 conda activate python3
 
+export OMP_NUM_THREADS=4
+export OPENBLAS_NUM_THREADS=6
+export MKL_NUM_THREADS=4
+export VECLIB_MAXIMUM_THREADS=4
+export NUMEXPR_NUM_THREADS=6
+
 python -m cbc_dp.index $*
